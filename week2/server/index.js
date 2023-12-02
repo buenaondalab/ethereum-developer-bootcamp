122dd47f7eec61ccbdeea589acdddb4f6586f40c
andrea <pierdominici.andrea@gmail.com>
2023-12-02 18:24:52 +0100

    hardcoded root

--- week2/server/index.js
+++ week2/server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-const MerkleTree = require('../utils/MerkleTree')
 
 const port = 1225;
 
@@ -9,7 +8,7 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = new MerkleTree(require('../utils/niceList.json')).getRoot();
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
