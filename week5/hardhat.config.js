5e3a6d0c196e45816f7e300085ef33545a908627
andrea <pierdominici.andrea@gmail.com>
2023-12-19 18:19:13 +0100

    config goerli network

--- week5/hardhat.config.js
+++ week5/hardhat.config.js
@@ -1,8 +1,16 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_BOOTCAMP_TEST_URL,
+      accounts: [process.env.ALCHEMY_BOOTCAMP_TEST_PK],
+    }
   }
+  
 };
