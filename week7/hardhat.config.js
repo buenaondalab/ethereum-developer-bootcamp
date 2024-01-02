a17549982e3ab548d9593aa3744f535e5dd17122
andrea <pierdominici.andrea@gmail.com>
2024-01-02 01:40:47 +0100

    configs

--- week7/hardhat.config.js
+++ week7/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_BOOTCAMP_TEST_URL,
+      accounts: [process.env.ALCHEMY_BOOTCAMP_TEST_PK],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  }
 };
